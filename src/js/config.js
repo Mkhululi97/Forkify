a974fc0c87fe1169e61e7e6a81e9e3476d2ba19b
mkhululi <mkhululimtshali@gmail.com>
2024-05-01 08:33:15 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '72f5f90b-f0e3-43f8-af90-df1a032690b1';
+export const MODAL_CLOSE_SEC = 2500;
